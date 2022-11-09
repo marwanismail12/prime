ec9e022d01ccf48817484eff870a5ebfc7d3a843
marwan <112703763+marwanismail12@users.noreply.github.com>
2022-11-09 19:39:58 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,28 +1,9 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-
-  int i, n;
-  bool is_prime = true;
-
-
-  cin >> n;
-
-  if (n == 0 || n == 1) {
-    is_prime = false;
-  }
-
-  for (i = 2; i <= 100/2; ++i) {
-    if (100 % i == 0 ) {
-      is_prime = false;
-      break;
-    }
-  }
-
-  if (is_prime)
-    cout << i;
-
-
-  return 0;
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
 }
